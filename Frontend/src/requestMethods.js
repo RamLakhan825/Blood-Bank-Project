f5d2c4f94cb976479d52be214009c772f73ba87b
RamLakhan825 <rljangid825@gmail.com>
2025-02-13 00:51:00 +0530

    Update requestMethods.js

--- Frontend/src/requestMethods.js
+++ Frontend/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://blood-bank-project-n8jv.vercel.app/";
+const BASE_URL = "https://blood-bank-project-n8jv.vercel.app/api/v1";
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
